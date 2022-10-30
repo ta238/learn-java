bed61f6811020ba1beef75d65b5474c697470213
lord.malgani3 <lord.malgani3@gmail.com>
2022-10-30 19:17:21 +0700

    bai ArrayList

--- HocMang/src/Mang.java
+++ HocMang/src/Mang.java
@@ -1,72 +1,77 @@
-//public class Mang {
+import com.sun.tools.javac.Main;
+
+import java.awt.desktop.SystemSleepEvent;
+import java.util.ArrayList;
+
+public class Mang {
 
     // BT1. trả về số lớn nhất trong mảng
     // [1, 3, 5, 4, 6, 7, 9, 1, 2, 4, 3 ,1 2, 4, 5, 6, 7, 8, 0]
     //  0  1  2  3  4  5
-   // public int timSoLonNhat(int[] mang) {
+    // public int timSoLonNhat(int[] mang) {
     // if (mang.length == 0) {
-     //       return 0;
-     //  }
+    //       return 0;
+    //  }
 
-      //  int x = mang[0];
-       // for (int i = 0; i < mang.length; i++) {
-      //      if (mang[i] > x) {
-       //         x = mang[i];
-       //     }
-      //  }
+    //  int x = mang[0];
+    // for (int i = 0; i < mang.length; i++) {
+    //      if (mang[i] > x) {
+    //         x = mang[i];
+    //     }
+    //  }
 
-   //     return x;
-   // }
+    //     return x;
+    // }
 
     // BT2. trả về tổng các phần tử của mảng
     //public int tinhTong(int[] mang) {
     //    int tong = 0;
     //    for (int i = 0; i < mang.length; i++) {
-     //       tong += mang[i];
-     //   }
-     //   return tong;
-  //  }
+    //       tong += mang[i];
+    //   }
+    //   return tong;
+    //  }
 
     // BT3. trả về vị tri (index) của số lơn nhất trong mảng
-   // public int timViTriSoLonNhat(int[] mang) {
-     //   int max = timSoLonNhat(mang);
+    // public int timViTriSoLonNhat(int[] mang) {
+    //   int max = timSoLonNhat(mang);
 
-   //     for (int j = 0; j < mang.length; j++) {
+    //     for (int j = 0; j < mang.length; j++) {
     //        if (max == mang[j]) {
-      //          return j;
-      //      }
-      //  }
+    //          return j;
+    //      }
+    //  }
 
-      //  return 0;
-   // }
+    //  return 0;
+    // }
 
     // 1, 2, 3, 4, 5, 6
-  //  public int timSoNhoNhat(int[] mang) {
+    //  public int timSoNhoNhat(int[] mang) {
     //    int x = 99999999;
-   //     for (int i = 0; i < mang.length; i++) {
-      //      if (mang[i] < x) {
-      //          x=mang[i] ;
-     //       }
-   //     }
+    //     for (int i = 0; i < mang.length; i++) {
+    //      if (mang[i] < x) {
+    //          x=mang[i] ;
+    //       }
+    //     }
     //    return x;
-   // }
+    // }
 
-  //  public int timSoLuongPhanTu(int[] mang, int n) {
-   //     int x=0;
-   //     for(int i =0;i<mang.length;i++){
+    //  public int timSoLuongPhanTu(int[] mang, int n) {
+    //     int x=0;
+    //     for(int i =0;i<mang.length;i++){
     //        if(mang[i]==n){
-     //           x++;
-     //       }
-     //   }
+    //           x++;
+    //       }
+    //   }
     //    return x;
-   // }
+    // }
 
-   // public int timSoLuongSoChan(int[] mang) {
-   //     int n=0;
-   //     for(int i =0;i<mang.length;i++){
-   //         if(mang[i]%2==0){
-     //           n++;
-     //       }
+    // public int timSoLuongSoChan(int[] mang) {
+    //     int n=0;
+    //     for(int i =0;i<mang.length;i++){
+    //         if(mang[i]%2==0){
+    //           n++;
+    //       }
     //    }
     //    return n;
     //}
@@ -74,23 +79,54 @@
     // [1, 4]
     // j = 0, mag = [1, 4], so luong so chan = 1, tong = 0; tong += 1 => tong = 1
     // j = 1, mang = [1, 4], so luong so chan = 1
-   // public int tinhTongCacSoChan(int[] mang) {
-   //     int tong=0;
+    // public int tinhTongCacSoChan(int[] mang) {
+    //     int tong=0;
     //    for(int j= 0;j<mang.length;j++) {
-   //         if (mang[j] % 2 == 0) {
+    //         if (mang[j] % 2 == 0) {
     //            tong += mang[j];
     //        }
-   //     }
-   //     return tong;
-   // }
+    //     }
+    //     return tong;
+    // }
 
-   // public int timSoLuongSoLonHon100(int[] mang) {
+    // public int timSoLuongSoLonHon100(int[] mang) {
     //    int n=0;
     //    for(int i = 0;i<mang.length;i++){
-     //       if(mang[i]>100){
-      //          n++;
-      //      }
-     //   }
-      //  return n;
-  //  }
-//}
+    //       if(mang[i]>100){
+    //          n++;
+    //      }
+    //   }
+    //  return n;
+    //  }
+
+    public static void main(String[] args) {
+        //tao mot ArrayList
+        ArrayList<Integer>list=new ArrayList<Integer>();
+        // them 1->10
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+        System.out.println(list);
+// in ra 3 phan tu dau tien
+        System.out.println(+list.get(0));
+        System.out.println(+list.get(1));
+        System.out.println(+list.get(2));
+        // xoa 3 phan tu dau tien
+        System.out.println("xoa 3 phan tu dau");
+        System.out.println(list.remove(0));
+        System.out.println(list.remove(0));
+        System.out.println(list.remove(0));
+        // in lai ra 3 phan tu dau tien
+        System.out.println("in lai ra 3 pan tu dau tien");
+        System.out.println(+list.get(0));
+        System.out.println(+list.get(1));
+        System.out.println(+list.get(2));
+    }
+}
--- HocMang/src/ViDumang.java
+++ HocMang/src/ViDumang.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
+
 public class ViDumang {
     // tra ve so lon nhat  trong mang
-    public int TimSoLonNhatTrongMang(int[]mang){
+    public int TimSoLonNhatTrongMang(int[] mang){
         int n=mang[0];
         for(int i=0;i<mang.length;i++){
             if(mang[i]>n){
@@ -9,6 +11,17 @@ public class ViDumang {
         }
         return n;
     }
+
+    public int TimSoLonNhatTrongMang(ArrayList<Integer> mang) {
+        int n = mang.get(0);
+        for(int i = 0; i < mang.size(); i++) {
+            if(mang.get(i) > n) {
+                n = mang.get(i);
+            }
+        }
+        return n;
+    }
+
 // tra ve tong cac pan tu trong mang
     public int TongCacPhanTuTrongmang(int []mang){
         int tong=0;
@@ -16,6 +29,17 @@ public class ViDumang {
             tong+=mang[i];
         }
         return tong;
+    }
+    public int TongCacPhanTuTrongMang(ArrayList<Integer>mang){
+        int tong= 0;
+        for (int i = 0; i < mang.size();i++){
+                tong+= mang.get(i);
+
+
+        }
+
+            return tong;
+
     }
     public void BaiTap1(int []mang){
         int mang1[]=mang.clone();
